477985846eab3cc13a4751a9b7776b10b00f0c8e
KSokhal <kulvir.sokhal@hotmail.com>
2019-12-30 23:36:20 +0000

    Working pathfinding

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from node import Grid
+from node import Node 
+from copy import deepcopy
 
 def main():
 
@@ -14,27 +17,84 @@ def main():
             ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"],
             ["O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O", "O"]
             ]
+    complete_grid = deepcopy(grid)
+    grid = Grid(grid)
 
-    start_pos = None
-    end_pos = None
+    open = []
+    closed = []
 
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if grid[row][col] == "S":
-                start_pos = (row, col)
-            elif grid[row][col] == "E":
-                end_pos = (row, col)
+    start_node = Node(grid, grid.start_pos)
+    start_node.calc_f_cost()
+    open.append(start_node)
+    current_node = start_node
+    while True:
+        current_node = open[0]
+        for node in open:
+            if node.f_cost == current_node.f_cost:
+                if node.h_cost < current_node.h_cost:
+                    current_node = node
+            if node.f_cost < current_node.f_cost:
+                current_node = node
+        
+        open.remove(current_node)
+        closed.append(current_node)
 
-def get_scores(start_pos, end_pos, pos):
-    g_cost = 0 # Distance from starting node
-    h_cost = 0 # Distance from end node
-    f_cost = 0 # g cost + h cost
-    
-    g_cost = ((pos[0] - start_pos[0])^2 + (pos[1] - start_pos[1])^2)^0.5
-    h_cost = ((pos[0] - end_pos[0])^2 + (pos[1] - end_pos[1])^2)^0.5
-    f_cost = g_cost + h_cost
 
-    return g_cost, h_cost, f_cost
+        if current_node.pos == grid.end_pos:
+            path_node = current_node
+            while path_node.parent is not None:  
+                pos = (path_node.parent.pos[0], path_node.parent.pos[1])
+                if complete_grid[pos[0]][pos[1]] == "S":
+                    break
+                complete_grid[pos[0]][pos[1]] = "P"
+                path_node = path_node.parent
+
+            break
+        
+        neighbour_nodes = get_neighbour_nodes(grid, current_node)
+
+        for neighbour in neighbour_nodes:
+            if grid.grid[neighbour.pos[0]][neighbour.pos[1]] == "W" or check_in(closed, neighbour):
+                continue
+
+
+            new_g_cost = current_node.g_cost + current_node.get_distance(current_node.pos,  neighbour.pos)
+
+            if new_g_cost < current_node.g_cost or not check_in(open, neighbour):
+                neighbour.g_cost = new_g_cost
+                neighbour.h_cost = neighbour.get_distance(neighbour.pos, grid.end_pos)
+                neighbour.calc_f_cost()
+                neighbour.parent = current_node
+                if not check_in(open, neighbour):
+                    open.append(neighbour)
+
+        print(current_node.pos)
+    print("found!")
+
+    for i in complete_grid:
+        print(i)
+
+def check_in(lst, node):
+    for n in lst:
+        if n.pos == node.pos:
+            return True
+    return False
+
+def get_neighbour_nodes(grid, node):
+    neighbour_nodes = []
+    for dx in range(-1, 2):
+        for dy in range(-1, 2):
+            if dx == 0 and dy == 0:
+                continue
+            else:
+                pos = (node.pos[0] + dx, node.pos[1] + dy)
+                next_node = Node(grid, pos)
+                neighbour_nodes.append(next_node)
+
+
+
+    return neighbour_nodes
+
 
 
 
--- node.py
+++ node.py
@@ -1,16 +1,36 @@
+import math
+
+class Grid:
+
+    def __init__(self, grid):
+        self.grid = grid
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if grid[row][col] == "S":
+                    self.start_pos = (row, col)
+                elif grid[row][col] == "E":
+                    self.end_pos = (row, col)
+
+
+
 class Node:
 
-    def __init__(pos, parent = None):
+    def __init__(self, Grid, pos, parent = None):
         self.pos = pos
-        self.g_cost = 0 # Distance from starting node
-        self.h_cost = 0 # Distance from end node
-        self.f_cost = 0 # g cost + h cost
+        #self.g_cost = math.sqrt( ( (self.pos[0] - Grid.start_pos[0]) ** 2 + (self.pos[1] - Grid.start_pos[1]) ** 2 ))   # Distance from starting node
+        #self.h_cost = math.sqrt( ( (Grid.end_pos[0]  - self.pos[0]) ** 2 + (Grid.end_pos[1] - self.pos[1] ) ** 2  ))   # Distance from end node
+        self.g_cost = self.get_distance(self.pos, Grid.start_pos)
+        self.h_cost = self.get_distance(self.pos, Grid.end_pos)
         if parent:
             self.parent = parent
+        else:
+            self.parent = None
 
+    @staticmethod
+    def get_distance(current_pos, reference_pos):
+        dx = abs(current_pos[0] - reference_pos[0])
+        dy = abs(current_pos[1] - reference_pos[1])
+        return (abs(dx-dy) * 1) + (min(dx, dy) * math.sqrt(2))
 
-    def get_costs(self, start_pos, end_pos):
-        self.g_cost = ((self.pos[0] - start_pos[0])^2 + (self.pos[1] - start_pos[1])^2)^0.5
-        self.h_cost = ((self.pos[0] - end_pos[0])^2 + (self.pos[1] - end_pos[1])^2)^0.5
-        self.f_cost = g_cost + h_cost
-
+    def calc_f_cost(self):
+        self.f_cost = self.g_cost + self.h_cost
